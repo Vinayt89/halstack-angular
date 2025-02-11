a495118d8de3708922adc90ead072f8bd4d3f742
vthakre2 <vthakre2@dxc.com>
2025-02-11 20:27:10 +0900

    11 Feb updated!

--- src/app/pages/screen-one/screen-one.component.ts
+++ src/app/pages/screen-one/screen-one.component.ts
@@ -29,17 +29,6 @@ export class ScreenOneComponent implements OnInit {
    isSectionVisible: boolean = false;
    isImageVisible: boolean = false;
    selectedOption: string = 'close';
-
-   loadTextData(fileName: string) {
-    this.generictextservice.readJSONFile(fileName).subscribe(
-      (data) => {
-        this.messages = data;
-      },
-      (error) => {
-        console.error('Error loading text file:', error);
-      }
-    );
-  }
    
    toggleSection(): void {
      this.isSectionVisible = !this.isSectionVisible;
@@ -50,7 +39,6 @@ export class ScreenOneComponent implements OnInit {
   }
 
   selectedSection: number | null = null;
-
   showSection(section: number): void {
     this.selectedSection = section;
     this.isSectionVisible = !this.isSectionVisible;
@@ -64,14 +52,89 @@ export class ScreenOneComponent implements OnInit {
     }
   }
 
-  step = 1;  // Current step
-  nextStep() {
-    if (this.step < 3) {
-      this.step++;
+  q1Selectlabels = ['生年月日'];
+
+  q2RadioBtn1Options = [
+    { id: '', label: 'ゴールド', value: '' }
+    ,{ id: '', label: 'ブルー', value: '' }
+    ,{ id: '', label: 'グリーン', value: '' }
+  ];
+
+  q2RadioBtn2Options = [
+    { id: '', label: '加入している', value: '加入している' }
+    ,{ id: '', label: '加入していない', value: '加入していない' }
+  ];
+
+  q2RadioBtn3Options = [
+    { id: '', label: '日常・レジャー', value: '日常・レジャー' }
+    ,{ id: '', label: '通勤・通学', value: '通勤・通学' }
+    ,{ id: '', label: '業務使用', value: '業務使用' }
+  ];
+
+  q2RadioBtn3SubOptions = [
+    { id: '', label: '3,000km以下', value: '3,000km以下' }
+    ,{ id: '', label: '3,000km超～5,000km以下', value: '3,000km超～5,000km以下' }
+    ,{ id: '', label: '5,000km超～10,000km以下', value: '5,000km超～10,000km以下' }
+    ,{ id: '', label: '10,000km超～15,000km以下', value: '10,000km超～15,000km以下' }
+    ,{ id: '', label: '15,000km超', value: '15,000km超' }
+  ];
+
+  q2RadioBtn4Options = [
+    { id: 1, label: '補償範囲：① 主に運転される方】のみ運転"', value: '補償範囲：①' }
+    ,{ id: 2, label: '補償範囲：①+② 【主に運転される方・配偶者・別居の未婚のお子さま】のみ運転', value: '補償範囲：①+②' }
+    ,{ id: 3, label: '補償範囲：①+②+③ 【同居のお子さま】以外の方が運転', value: '補償範囲：①+②+③' }
+    ,{ id: 4, label: '補償範囲：①+②+③+④ 【同居のお子さま（その方の配偶者含む）】も含め運転', value: '補償範囲：①+②+③+④' }
+  ];
+
+  selectedValue: string = '';
+  visibleSection: string = '';
+  q4ClickEvent(value: string) {
+    switch (value) {
+      case '日常・レジャー':
+        this.visibleSection = 'q4subsection';
+        this.showSection(1);
+        break;
+      case '通勤・通学':
+        this.visibleSection = 'q4subsection';
+        this.showSection(1);
+        break;
+      case '業務使用':
+        this.visibleSection = 'q4subsection';
+        this.showSection(2);
+        break;
+      default:
+        this.visibleSection = '';
     }
   }
-  
-  q1Selectlabels = ['生年月日'];
+
+
+  q5ClickEvent(value: string) {
+    switch (value) {
+      case '補償範囲：①':
+        this.setActiveImage(1);
+        break;
+      case '補償範囲：①+②':
+        this.setActiveImage(2);
+        break;
+      case '補償範囲：①+②+③':
+        this.setActiveImage(3);
+        break;
+      case '補償範囲：①+②+③+④':
+        this.setActiveImage(4);
+        break;
+    }
+  }
+
+  loadTextData(fileName: string) {
+    this.generictextservice.readJSONFile(fileName).subscribe(
+      (data) => {
+        this.messages = data;
+      },
+      (error) => {
+        console.error('Error loading text file:', error);
+      }
+    );
+  }
 
   footerLinks = [
     { text: '会社情報', href: 'https://www.sompo-direct.co.jp/shisan_company/company.html' },
